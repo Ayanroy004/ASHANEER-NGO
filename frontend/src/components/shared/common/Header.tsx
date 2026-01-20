055be1bfda64ee5f8fa46aa7a0b2d31132e31c13
Ayan Roy <154493815+Ayanroy004@users.noreply.github.com>
2026-01-20 20:37:33 +0530

    Header add dropdown multiple services

--- frontend/src/components/shared/common/Header.tsx
+++ frontend/src/components/shared/common/Header.tsx
@@ -1,10 +1,29 @@
 import { Menu } from "antd";
+import { AiOutlineDown } from "react-icons/ai";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const items = [
   { key: "/", label: "Home" },
   { key: "/about", label: "ABOUT US" },
-  { key: "/services", label: "SERVICES" },
+  {
+    key: "/services",
+    label: (
+      <div className="flex items-center justify-between">
+        SERVICES <span className="text-xs ps-1">{<AiOutlineDown />}</span>
+      </div>
+    ),
+    children: [
+      { key: "/old-age-home", label: "Old Age Home" },
+      { key: "/learning-kids", label: "Learning of kids" },
+      { key: "/food-dist", label: "Food distribution to needy" },
+      {
+        key: "/medical-support",
+        label: "Medical support to rural areas",
+      },
+      { key: "/tree-plant", label: "Tree plantation" },
+      { key: "/service6", label: "Services 6" },
+    ],
+  },
   { key: "/contact-us", label: "CONTACT US" },
 ];
 
